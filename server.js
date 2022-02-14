f4acb3bb4ca2fa8ce32a9cbe83fb08bdf891396b
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-13 22:25:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ function getUsers() {
     })
     .catch(error => console.log(error))
 }
+
+function getUser() {
+  axios.get(`${url}/user-`)
+    .then(response => {
+      const data = response.data
+
+      userAvatar.src = data.avatar
+      userNome.textContent = data.name
+      userId.textContent = data.id
+      userCity.textContent = data.city
+    })
+    .catch(error => console.log(error))
+}
